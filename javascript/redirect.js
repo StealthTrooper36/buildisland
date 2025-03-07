359a95888f72803d0857d8c2fc7f5fa3a4f429f6
StealthTrooper36 <StealthTrooper36@gmail.com>
2025-03-07 16:23:38 +0000

    sss

--- javascript/redirect.js
+++ javascript/redirect.js
@@ -1,9 +1,11 @@
 (function() {
-    if (window.location.hostname === 'stealthtrooper36.github.io' && 
-        window.location.pathname.startsWith('/buildisland')) {
-      const currentPath = window.location.pathname.replace('/buildisland', '');
-      const newUrl = 'https://buildisland.pages.dev' + (currentPath || '/');
+  if (window.location.hostname === 'stealthtrooper36.github.io') {
+    const currentPath = window.location.pathname;
+    if (currentPath.startsWith('/buildisland')) {
+      const newPath = currentPath.replace('/buildisland', '');
+      const newUrl = 'https://buildisland.pages.dev' + (newPath || '/');
       const queryAndHash = window.location.search + window.location.hash;
       window.location.replace(newUrl + queryAndHash);
     }
-  })();
+  }
+})();
